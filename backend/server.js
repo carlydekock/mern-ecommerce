a35ee4356403644f0eb2476ab34294a85b69118e
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-10-22 14:14:54 -0700

    transferred everything to es modules

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express = require('express');
-require('dotenv').config();
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+
+dotenv.config();
 
 const app = express();
 
