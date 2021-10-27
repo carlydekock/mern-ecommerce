c9b8bbb1a67b160c1b95a466e3d32b8e856fb9fe
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-10-27 12:07:13 -0700

    brought functions into product controller to clean up routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,26 +1,11 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
+import { getProductById, getProducts } from '../controllers/productController.js';
 const router = express.Router();
-import Product from '../models/productModel.js';
 
 //Fetch all products, GET /api/products, access:public
-router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-
-  res.json(products);
-}));
+router.route('/').get(getProducts);
 
 //Fetch a single product, GET /api/products/:id, access:public
-router.get('/:id', asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
-
-  if(product){
-    res.json(product);
-  } else {
-    res.status(404);
-    throw new Error('Product not found');
-  }
-  
-}));
+router.route('/:id').get(getProductById);
 
 export default router;
