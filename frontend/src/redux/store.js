1f0c946ce1d718459f65e80c34474ff6277259a4
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-11-02 10:53:53 -0700

    shipping screen created, save shipping added to redux

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -17,8 +17,9 @@ const reducer = combineReducers({
 
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
 const userInfoFromLocalStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
+const shippingAddressFromLocalStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {};
 const initialState = {
-  cart: { cartItems: cartItemsFromLocalStorage },
+  cart: { cartItems: cartItemsFromLocalStorage, shippingAddress: shippingAddressFromLocalStorage },
   userLogin: { userInfo: userInfoFromLocalStorage},
 };
 const middleware = [thunk];
