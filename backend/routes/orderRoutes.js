65d4ee53f485207da20b6f42cb93f12675898d87
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-11-13 16:58:37 -0800

    order paid route endpoint added

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
 
-import { createOrder, getOrderById } from '../controllers/orderController.js';
+import { createOrder, getOrderById, updateOrderToPaid } from '../controllers/orderController.js';
 import { protectRoute } from '../middleware/authMiddleware.js';
 
 router.route('/').post(protectRoute, createOrder);
 router.route('/:id').get(protectRoute, getOrderById);
+router.route('/:id/pay').put(protectRoute, updateOrderToPaid);
 
 export default router;
