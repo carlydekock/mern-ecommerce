a56c567ae12e6596505de30ae35d79a48f5cc20a
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-11-14 19:43:54 -0800

    create and update endpoints created

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import { getProductById, getProducts, deleteProduct } from '../controllers/productController.js';
+import { getProductById, getProducts, deleteProduct, createProduct, updateProduct } from '../controllers/productController.js';
 import { protectRoute, isAdmin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 //Fetch all products, GET /api/products, access:public
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protectRoute, isAdmin, createProduct);
 
 //Fetch a single product, GET /api/products/:id, access:public
-router.route('/:id').get(getProductById).delete(protectRoute, isAdmin, deleteProduct);
+router.route('/:id').get(getProductById).delete(protectRoute, isAdmin, deleteProduct).put(protectRoute, isAdmin, updateProduct);
 
 export default router;
