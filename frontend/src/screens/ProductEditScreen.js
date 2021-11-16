c47e9843759d7d3ffa04234b4b0b485d2c42fa70
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-11-16 15:12:32 -0800

    front-end image upload capability functioning

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer';
-import { listProductDetails } from '../redux/actions/productActions';
+import { listProductDetails, updateProduct } from '../redux/actions/productActions';
+import { PRODUCT_DETAILS_RESET, PRODUCT_UPDATE_RESET } from '../redux/constants/productContants';
 
 
 const ProductEditScreen = ({ match, history }) => {
@@ -18,29 +20,71 @@ const ProductEditScreen = ({ match, history }) => {
   const [category, setCategory] = useState('');
   const [countInStock, setCountInStock] = useState(0);
   const [description, setDescription] = useState('');
+  const [uploading, setUploading] = useState(false);
 
   const dispatch = useDispatch();
 
   const productDetails = useSelector(state => state.productDetails);
   const { loading, error, product } = productDetails;
 
+  const productUpdate = useSelector(state => state.productUpdate);
+  const { loading:loadingUpdate, error:errorUpdate, success:successUpdate } = productUpdate;
+
   useEffect(() => {
-    if(!product.name || product._id !== productId){
-      dispatch(listProductDetails(productId));
+    if(successUpdate){
+      dispatch({type: PRODUCT_UPDATE_RESET});
+      dispatch({type: PRODUCT_DETAILS_RESET});
+      history.push('/admin/productlist');
     } else {
-      setName(product.name);
-      setPrice(product.price);
-      setImage(product.image);
-      setBrand(product.brand);
-      setCategory(product.category);
-      setCountInStock(product.countInStock);
-      setDescription(product.description);
+      if(!product.name || product._id !== productId){
+        dispatch(listProductDetails(productId));
+      } else {
+        setName(product.name);
+        setPrice(product.price);
+        setImage(product.image);
+        setBrand(product.brand);
+        setCategory(product.category);
+        setCountInStock(product.countInStock);
+        setDescription(product.description);
+      }
     }
-  }, [dispatch, history, productId, product]);
+  }, [dispatch, history, productId, product, successUpdate]);
   
   const handleSubmit = (e) => {
     e.preventDefault();
     //update product details
+    dispatch(updateProduct({
+      _id: productId,
+      name,
+      price,
+      image,
+      brand,
+      category,
+      description,
+      countInStock,
+    }));
+  }
+
+  const handleFileUpload = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('image', file);
+    setUploading('true');
+
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+
+      const { data } = await axios.post('/api/upload', formData, config);
+      setImage(data);
+      setUploading(false);
+    } catch(error){
+      console.error(error);
+      setUploading(false);
+    }
   }
 
   return (
@@ -48,6 +92,8 @@ const ProductEditScreen = ({ match, history }) => {
       <Link to='/admin/productlist' className='btn btn-light my-3'>Go Back</Link>
       <FormContainer>
         <h1>Edit Product</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
         {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
         <Form onSubmit={handleSubmit}>
         <Form.Group controlId='name' className='my-3'>
@@ -56,11 +102,14 @@ const ProductEditScreen = ({ match, history }) => {
           </Form.Group>
           <Form.Group controlId='price' className='my-3'>
             <Form.Label>Price</Form.Label>
-            <Form.Control type='number' placeholder='Enter price' value={price.toFixed(2)} onChange={(e) => setPrice(e.target.value)}></Form.Control>
+            <Form.Control type='number' placeholder='Enter price' value={price} onChange={(e) => setPrice(e.target.value)}></Form.Control>
           </Form.Group>
           <Form.Group controlId='image' className='my-3'>
-            <Form.Label>Image</Form.Label>
-            <Form.Control type='text' placeholder='Enter image url' value={image} onChange={(e) => setImage(e.target.value)}></Form.Control>
+            <Form.Label>Image URL</Form.Label>
+            <Form.Control type='file' placeholder='Enter image url' onChange={handleFileUpload}></Form.Control>
+            {/* <Form.Control type='text' placeholder='Enter image url' value={image} onChange={(e) => setImage(e.target.value)}></Form.Control>
+            <Form.File id='image-file' label='Choose File' custom onChange={handleFileUpload}></Form.File> */}
+            {uploading && <Loader />}
           </Form.Group>
           <Form.Group controlId='brand' className='my-3'>
             <Form.Label>Brand</Form.Label>
@@ -73,12 +122,6 @@ const ProductEditScreen = ({ match, history }) => {
           <Form.Group controlId='category' className='my-3'>
             <Form.Label>Category</Form.Label>
             <Form.Control type='text' placeholder='Enter category name' value={category} onChange={(e) => setCategory(e.target.value)}></Form.Control>
-            {/* <Form.Select size="sm">
-              <option value={category}>Gear</option>
-              <option value={category}>Clothing</option>
-              <option value={category}>Accessories</option>
-              <option value={category}>Equipment</option>
-            </Form.Select> */}
           </Form.Group>
           <Form.Group controlId='description' className='my-3'>
             <Form.Label>Description</Form.Label>
