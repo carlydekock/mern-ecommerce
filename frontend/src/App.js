2071518470436f8e6c2c9ff0026cd473e9be245d
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-11-18 12:34:17 -0800

    search box form rendering in header and search functionality working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,6 +26,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Route exact path="/" component={HomeScreen} />
+          <Route path="/search/:keyword" component={HomeScreen} />
           <Route path="/register" component={RegisterScreen} />
           <Route path="/login" component={LoginScreen} />
           <Route path="/profile" component={ProfileScreen} />
--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -6,11 +6,11 @@ import {PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_FAIL} fro
 import {PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAIL} from '../constants/productContants.js';
 import {PRODUCT_CREATE_REVIEW_REQUEST, PRODUCT_CREATE_REVIEW_SUCCESS, PRODUCT_CREATE_REVIEW_FAIL} from '../constants/productContants.js';
 
-export const listProducts = () => async (dispatch) =>{
+export const listProducts = (keyword = '') => async (dispatch) =>{
   try {
     dispatch({type: PRODUCT_LIST_REQUEST});
 
-    const { data} = await axios.get('/api/products');
+    const { data} = await axios.get(`/api/products?keyword=${keyword}`);
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
