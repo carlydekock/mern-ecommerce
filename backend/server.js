4ca6716072eb0e5d4ad6dee6d5f4fbe79a353b37
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-11-18 11:17:22 -0800

    create review endpoint and functionality added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import morgan from 'morgan';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import connectDB from './config/db.js';
@@ -13,6 +14,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev')); //gives http method, status, etc logged to the console
+}
 app.use(express.json());
 
 app.get('/', (req, res) => {
