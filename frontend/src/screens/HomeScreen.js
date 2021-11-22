6ca2d7027bbc46a4e4092f508440f5424f92e266
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-11-21 18:37:54 -0800

    added link to go back after searching on home page

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import Message from '../components/Message';
@@ -23,7 +24,7 @@ const HomeScreen = ({match}) => {
 
   return (
     <>
-    {!keyword && <ProductCarousel />}
+    {!keyword ? <ProductCarousel /> : <Link to='/' className='btn btn-light'>Go Back</Link>}
       <h1>Latest Products</h1>
       {loading ? (
         <Loader />
